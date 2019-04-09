5a7458e80b59e02ac375d204d51c329a8a57eeae
Joost de Lange <joost@index.nl>
2019-04-09 16:10:02 +0200

    Added package, instructions and updated vuforia

--- Assets/Scripts/Restart.cs
+++ Assets/Scripts/Restart.cs
@@ -3,7 +3,28 @@ using UnityEngine.SceneManagement;
 
 public class Restart : MonoBehaviour
 {
+    public GameObject instructions1;
+    public GameObject instructions2;
+    public GameObject instructions3;
+    
     public void RestartGame() {
         SceneManager.LoadScene("Main");
+    }
+
+    public void DismissInstructions1()
+    {
+        instructions1.SetActive(false);
+        instructions2.SetActive(true);
+    }
+    
+    public void DismissInstructions2()
+    {
+        instructions2.SetActive(false);
+        instructions3.SetActive(true);
+    }
+    
+    public void DismissInstructions3()
+    {
+        instructions3.SetActive(false);
     }
 }
